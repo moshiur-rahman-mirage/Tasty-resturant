7f6786fb6ceac49355f2b41e44ec047be6be3d31
moshiur-rahman-mirage <moshiur.mirage@gmail.com>
2023-11-08 19:37:34 +0600

    .env implemented

--- src/Firebase/Firebase.config.js
+++ src/Firebase/Firebase.config.js
@@ -1,14 +1,14 @@
-// Import the functions you need from the SDKs you need
+
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBhhX5gpDXaKRX5y0mk__KL0cEHoic7VVI",
-    authDomain: "brandshop-c5e49.firebaseapp.com",
-    projectId: "brandshop-c5e49",
-    storageBucket: "brandshop-c5e49.appspot.com",
-    messagingSenderId: "966359573519",
-    appId: "1:966359573519:web:17129c5de7c593a2512f7a"
-  };
+  apiKey: process.env.apiKey,
+  authDomain: process.env.authDomain,
+  projectId: process.env.projectId,
+  storageBucket: process.env.storageBucket,
+  messagingSenderId: process.env.messagingSenderId,
+  appId: process.env.appId
+};
 
 
 const app = initializeApp(firebaseConfig);
